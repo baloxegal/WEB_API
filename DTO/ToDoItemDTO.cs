a0770b55e06d519bbfd8fbcb01ed1067fabea606
baloxegal <baloxegal@gmail.com>
2021-04-14 22:29:58 +0300

    Add project files.

---
+++ DTO/ToDoItemDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_API.Models;
+
+namespace WEB_API.DTO
+{
+    public class ToDoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public ToDoItemDTO(TodoItem toDoItem)
+        {
+            Id = toDoItem.Id;
+            Name = toDoItem.Name;
+            IsComplete = toDoItem.IsComplete;
+        }
+    }
+}
